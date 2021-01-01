b88aa5160523c325be87cadd76adb3f21fdc4b12
wellington <wellingtonslpbi@hotmail.com>
2021-01-01 17:13:18 -0300

    creating post component ui

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
 import Video from 'react-native-video';
+
+import Entypo from 'react-native-vector-icons/Entypo';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import styles from './styles';
 
 const Post = () => {
+  const [paused, setPaused] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Video
-        source={{
-          uri:
-            'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-        }}
-        resizeMode="cover"
-        onError={(e) => console.log(e)}
-        style={styles.video}
-      />
+      <TouchableWithoutFeedback onPress={() => setPaused(!paused)}>
+        <Video
+          source={{
+            uri:
+              'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+          }}
+          resizeMode="cover"
+          onError={(e) => console.log(e)}
+          paused={paused}
+          repeat
+          style={styles.video}
+        />
+      </TouchableWithoutFeedback>
+
+      <View style={styles.uiContainer}>
+        <View style={styles.rightContainer}>
+          <View style={styles.profileImage}>
+            <Image source={''} />
+          </View>
+
+          <TouchableOpacity style={styles.iconContainer} onPress={() => {}}>
+            <AntDesign name={'heart'} size={30} color={'white'} />
+            <Text style={styles.statsLabel}>123</Text>
+          </TouchableOpacity>
+
+          <View style={styles.iconContainer}>
+            <FontAwesome name={'commenting'} size={30} color="white" />
+            <Text style={styles.statsLabel}>500</Text>
+          </View>
+
+          <View style={styles.iconContainer}>
+            <Fontisto name={'share-a'} size={25} color="white" />
+            <Text style={styles.statsLabel}>211</Text>
+          </View>
+        </View>
+
+        <View style={styles.bottomContainer}>
+          <View>
+            <Text style={styles.author}>@daviddobric</Text>
+            <Text style={styles.description}>hahaha oh boy @borat</Text>
+
+            <View style={styles.songRow}>
+              <Entypo name="beamed-note" size={24} color="white" />
+              <Text style={styles.songName}>Nf - the search</Text>
+            </View>
+          </View>
+
+          <Image style={styles.songImage} source={''} />
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -12,6 +12,71 @@ const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
   },
+  uiContainer: {
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  rightContainer: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    paddingRight: 8,
+  },
+  profileImage: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    borderWidth: 2,
+    borderColor: '#fff',
+    marginBottom: 20,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  statsLabel: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  bottomContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 150,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  author: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  description: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '300',
+    marginBottom: 8,
+  },
+  songRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  songName: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 6,
+  },
+  songImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 5,
+    borderColor: '#4c4c4c',
+  },
 });
 
 export default styles;
